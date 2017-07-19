70af09a9940751e20d98c15b881286fd7c84de28
Anton Reshetov <reshetov.art@gmail.com>
2017-07-19 09:48:17 +0300

    Default programmatic true props

--- src/package/alert/alert.js
+++ src/package/alert/alert.js
@@ -4,9 +4,16 @@ import VuAlert from './Alert.vue'
 export default {
     open (params) {
         const AlertConstructor = Vue.extend(VuAlert)
+
+        const defaultParams = {
+            programmatic: true
+        }
+
+        const propsData = Object.assign(defaultParams, params)
+
         return new AlertConstructor({
             el: document.createElement('div'),
-            propsData: params
+            propsData
         })
     }
 }

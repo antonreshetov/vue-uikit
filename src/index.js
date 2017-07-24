3bf28ca0b0a74069ab590fb81a6f3a62232cd71e
Anton Reshetov <reshetov.art@gmail.com>
2017-07-24 06:41:20 +0300

    Import table component, lodash

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import VuAlert from './package/alert/Alert.vue'
 import VuAlertFn from './package/alert/alert'
+import VuTable from './package/table/Table.vue'
+import VuTableColumn from './package/table/Column.vue'
 
 const components = [
-    VuAlert
+    VuAlert,
+    VuTable,
+    VuTableColumn
 ]
+
+window._ = require('lodash')
 
 const VueUIkit = {
     install (Vue, options) {

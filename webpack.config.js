020255e3922ecf7a923240f86c1ecba0e3f14cc8
Anton Reshetov <reshetov.art@gmail.com>
2017-08-05 09:47:47 +0300

    Fix spelling

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist/',
-        filename: 'expample.build.js'
+        filename: 'example.build.js'
     },
     module: {
         rules: [

c0cef8bc9a7c99b8cd0a5a646d921ca392205ea4
Anton Reshetov <reshetov.art@gmail.com>
2017-08-15 10:07:47 +0300

    Copy docs, create .htaccess

---
+++ build/copy.js
@@ -0,0 +1,15 @@
+const fs = require('fs-extra')
+const colors = require('colors')
+
+const htaccess = `RewriteEngine on
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteRule . index.html [L]`
+
+fs.copy('./docs', './dist/docs')
+    .then(() => {
+        fs.writeFile('./dist/.htaccess', htaccess)
+            .then(() => console.log(colors.green('\nFiles copy success')))
+            .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))

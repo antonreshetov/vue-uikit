a73bcad5524a20068c1a7e1551d04690622a8fcf
Anton Reshetov <reshetov.art@gmail.com>
2017-10-01 00:25:50 +0300

    Added new component - button

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import VuAccordion from './package/accordion/Accordion.vue'
 import VuAccordionItem from './package/accordion/Item.vue'
 import VuAlert from './package/alert/Alert.vue'
 import VuAlertFn from './package/alert/alert'
+import VuButton from './package/button/Button.vue'
 
 const components = [
     VuAccordion,
     VuAccordionItem,
-    VuAlert
+    VuAlert,
+    VuButton
 ]
 
 const VueUIkit = {
---
+++ src/package/button/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="uk-button uk-button-default">
+        <slot></slot>
+    </button>
+</template>
+<script>
+export default {
+    name: 'VuButton',
+
+    props: {},
+
+    data () {
+        return {}
+    }
+}
+</script>

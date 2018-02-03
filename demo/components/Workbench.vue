80dba61156daf60db05be9d1c1e3b8d52eec06b4
Anton Reshetov <reshetov.art@gmail.com>
2018-02-03 10:46:05 +0300

    Add: Workbench for testing of components

---
+++ demo/components/Workbench.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="workbench">
+    <h1>Workbench</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.workbench {
+  h1 {
+    text-align: center;
+  }
+  padding: 20px;
+}
+</style>
